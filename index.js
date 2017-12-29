75b074a738b7166df6cda8c64d30e34ce1dd73ec
JSpeed8th <jspeed8th@gmail.com>
2017-12-29 22:54:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
 
 function printBadges (employeeName) {
   for (i = 0; i < employeeName.length; i++) {
-    console.log(`Welcome ${employeeName[i]}! You are employee #${i}.`)
+    console.log(`Welcome ${employeeName[i]}! You are employee #${employeeName.length}.`);
   }
 
   return employeeName;
