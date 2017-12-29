81c507f50eb31386d65cb5caf7bac41ece556099
JSpeed8th <jspeed8th@gmail.com>
2017-12-29 22:40:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-// Code your solutions in this file
-function printBadges(employeeNames) {
-  for (let e = 0; e < employeeNames.length; e++) {
-    console.log(`Welcome ${employeeNames[e]}! You are employee #${e + 1}.`);
-  }
-  return employeeNames;
-}
+// // Code your solutions in this file
+// function printBadges(employeeNames) {
+//   for (let e = 0; e < employeeNames.length; e++) {
+//     console.log(`Welcome ${employeeNames[e]}! You are employee #${e + 1}.`);
+//   }
+//   return employeeNames;
+// }
+// 
+// function tailsNeverFails() {
+//   let counter = 0;
+//   while (Math.random() >= 0.5){
+//     console.log('Tails!')
+//     counter++;
+//   }
+//   console.log('Heads!')
+//   return `You got ${counter} tails in a row!`;
+// }
 
-function tailsNeverFails() {
-  let counter = 0;
-  while (Math.random() >= 0.5){
-    console.log('Tails!')
-    counter++;
-  }
-  console.log('Heads!')
-  return `You got ${counter} tails in a row!`;
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
 }
